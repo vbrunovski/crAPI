7164f4dc353171e304d5c3e1bda52b59f90e7edd
Roshan Piyush <roshan.piyush@traceable.ai>
2024-12-09 11:08:19 +0000

    Fix convention

--- services/identity/entrypoint.sh
+++ services/identity/entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 
 if [ -f /keys/jwks.json ]; then
-  JWKS=$(openssl base64 -in /keys/jwks.json -A)
+  JWKS=$(openssl base64 -in /app/keys/jwks.json -A)
 else
   echo "Loading default JWKS file."
-  JWKS=$(openssl base64 -in /default_jwks.json -A)
+  JWKS=$(openssl base64 -in /app/default_jwks.json -A)
 fi
 java -jar /app/identity-service-1.0-SNAPSHOT.jar --app.jwksJson=$JWKS
 
