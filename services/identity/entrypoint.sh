8eee34aed96a24063c103e6f5c297318fb0b485f
Roshan Piyush <piyush.roshan@gmail.com>
2025-02-03 18:39:12 +0530

    Fix health of gateway image (#289)
    * Fix health of gateway image
    * Update docker compose

--- services/identity/entrypoint.sh
+++ services/identity/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 
-if [ -f /keys/jwks.json ]; then
+if [ -f /app/keys/jwks.json ]; then
+  echo "Loading JWKS key file /app/keys/jwks.json"
   JWKS=$(openssl base64 -in /app/keys/jwks.json -A)
 else
   echo "Loading default JWKS file."
